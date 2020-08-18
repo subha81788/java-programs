0ae7ca4a0142045228a53a8abf3a01c74fcb9664
subha81788 <subha81788@gmail.com>
2020-08-18 20:29:01 +0530

    updated Intercepting Filter pattern

--- PatternInterceptingFilter.java
+++ PatternInterceptingFilter.java
@@ -3,13 +3,61 @@ import java.util.List;
 
 public class PatternInterceptingFilter {
     public static void main(String[] args) {
-        var chain = new FilterChain(new Target());
+        var target = new Target();
+
+        var chain = new FilterChain();
         chain.addFilter(new AuthenticationFilter());
         chain.addFilter(new LogFilter());
 
-        var client = new Client();
+        var client = new Client(target);
         client.setFilterChain(chain);
         client.sendRequest("Downloading");
+    }
+}
+
+class Client {
+    private Target target;
+    private FilterChain chain;
+
+    public Client(Target target) {
+        this.target = target;
+    }
+
+    public void setFilterChain(FilterChain chain) {
+        this.chain = chain;
+        this.chain.setTarget(this.target);
+    }
+
+    public void sendRequest(String request) {
+        System.out.println("Sending " + request + " request to target...");
+        this.chain.doFilter(request);
+    }
+}
+
+class Target {
+    public void execute(String request) {
+        System.out.println("Target executed " + request);
+    }
+}
+
+class FilterChain {
+    private Target target;
+    private List<Filter> filters = new ArrayList<>();
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
+
+    public void addFilter(Filter filter) {
+        filters.add(filter);
+    }
+
+    public void doFilter(String request) {
+        // this is an example of pre-filter
+        System.out.println("Pre processing " + request + " request");
+        filters.stream().forEach(f -> f.apply(request));
+        // passes the request to target for actual execution
+        this.target.execute(request);
     }
 }
 
@@ -28,41 +76,5 @@ class LogFilter implements Filter {
     @Override
     public void apply(String request) {
         System.out.println("Logging " + request);
-    }
-}
-
-class Target {
-    public void execute(String request) {
-        System.out.println("Target executing " + request);
-    }
-}
-
-class FilterChain {
-    private List<Filter> filters = new ArrayList<>();
-    private Target target;
-
-    public FilterChain(Target target) {
-        this.target = target;
-    }
-
-    public void addFilter(Filter filter) {
-        filters.add(filter);
-    }
-
-    public void execute(String request) {
-        filters.stream().forEach(f -> f.apply(request));
-        this.target.execute(request);
-    }
-}
-
-class Client {
-    private FilterChain chain;
-
-    public void setFilterChain(FilterChain chain) {
-        this.chain = chain;
-    }
-    public void sendRequest(String request) {
-        System.out.println("Sending " + request + " request to target");
-        this.chain.execute(request);
     }
 }
