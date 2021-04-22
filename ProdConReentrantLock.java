9fd35855414632c0cd5c9d9c6bbfcaabb3bf8759
subha81788 <subha81788@gmail.com>
2021-04-22 09:58:30 +0530

    Update ProdConReentrantLock.java

--- ProdConReentrantLock.java
+++ ProdConReentrantLock.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.TimeUnit;
 
 public class ProdConReentrantLock {
 
@@ -15,8 +16,8 @@ public class ProdConReentrantLock {
         // creating producer and consumer threads
         Runnable prod = new Producer("PRODUCER", sharedResource);
         Runnable con = new Consumer("CONSUMER", sharedResource);
-		Thread prodThread = new Thread(prod);
-		Thread conThread = new Thread(con);
+        Thread prodThread = new Thread(prod);
+        Thread conThread = new Thread(con);
 
         // starting producer and consumer threads
         prodThread.start();
@@ -28,7 +29,7 @@ public class ProdConReentrantLock {
 class Buffer {
     // producer consumer problem data
     private static final int CAPACITY = 10;
-    private final Queue<Integer> queue = new LinkedList<>();
+    private final Queue < Integer > queue = new LinkedList < > ();
     private final Random theRandom = new Random();
 
     // lock and condition variables
@@ -37,74 +38,76 @@ class Buffer {
     private final Condition bufferNotEmpty = theLock.newCondition();
 
     public void put() throws InterruptedException {
-		while(true) {
-			theLock.lock();
-			try {
-				while (queue.size() == CAPACITY) {
-					System.out.println(Thread.currentThread().getName()
-							+ " : Buffer is full, waiting");
-					bufferNotEmpty.await();
-				}
+        while (true) {
+            if (theLock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    while (queue.size() == CAPACITY) {
+                        System.out.println(Thread.currentThread().getName() +
+                            " : Buffer is full, waiting");
+                        bufferNotEmpty.await();
+                    }
 
-				int number = theRandom.nextInt(1000);
-				
-				queue.add(number);
-				System.out.printf("%s produced %d into queue %n", Thread
-							.currentThread().getName(), number);
+                    int number = theRandom.nextInt(1000);
 
-				// signal consumer thread that, buffer has element now
-				System.out.println(Thread.currentThread().getName()
-							+ " : Signalling that buffer is full now");
-				bufferNotFull.signalAll();
-				try {
-                    Thread.sleep(100);
-                } catch(InterruptedException e) {
-                    System.err.println(e);   
+                    queue.add(number);
+                    System.out.printf("%s produced %d into queue %n", Thread
+                        .currentThread().getName(), number);
+
+                    // signal consumer thread that, buffer has element now
+                    System.out.println(Thread.currentThread().getName() +
+                        " : Signalling that buffer is full now");
+                    bufferNotFull.signalAll();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                        System.err.println(e);
+                    }
+                } finally {
+                    theLock.unlock();
                 }
-			} finally {
-				theLock.unlock();
-			}
-		}
+            }
+        }
     }
 
     public void get() throws InterruptedException {
-		while(true) {
-			theLock.lock();
-			try {
-				while (queue.size() == 0) {
-					System.out.println(Thread.currentThread().getName()
-							+ " : Buffer is empty, waiting");
-					bufferNotFull.await();
-				}
+        while (true) {
+            if (theLock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    while (queue.size() == 0) {
+                        System.out.println(Thread.currentThread().getName() +
+                            " : Buffer is empty, waiting");
+                        bufferNotFull.await();
+                    }
 
-				Integer value = queue.poll();
-				if (value != null) {
-					System.out.printf("%s consumed %d from queue %n", Thread
-							.currentThread().getName(), value);
+                    Integer value = queue.poll();
+                    if (value != null) {
+                        System.out.printf("%s consumed %d from queue %n", Thread
+                            .currentThread().getName(), value);
 
-					// signal producer thread that, buffer may be empty now
-					System.out.println(Thread.currentThread().getName()
-							+ " : Signalling that buffer may be empty now");
-					bufferNotEmpty.signalAll();
-				}
-				try {
-                    Thread.sleep(300);
-                } catch(InterruptedException e) {
-                    System.err.println(e);   
+                        // signal producer thread that, buffer may be empty now
+                        System.out.println(Thread.currentThread().getName() +
+                            " : Signalling that buffer may be empty now");
+                        bufferNotEmpty.signalAll();
+                    }
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(300);
+                    } catch (InterruptedException e) {
+                        System.err.println(e);
+                    }
+                } finally {
+                    theLock.unlock();
                 }
-			} finally {
-				theLock.unlock();
-			}
-		}
+            }
+        }
     }
 }
 
 class Producer implements Runnable {
-	String name;
+    String name;
     Buffer sharedResource;
 
     public Producer(String name, Buffer sharedResource) {
-		this.name = name;
+        this.name = name;
         this.sharedResource = sharedResource;
     }
 
@@ -119,11 +122,11 @@ class Producer implements Runnable {
 }
 
 class Consumer implements Runnable {
-	String name;
+    String name;
     Buffer sharedResource;
 
     public Consumer(String name, Buffer sharedResource) {
-		this.name = name;
+        this.name = name;
         this.sharedResource = sharedResource;
     }
 
