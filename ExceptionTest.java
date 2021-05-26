3261dcefe7416cb9f290f7339b590ca30109d548
subha81788 <subha81788@gmail.com>
2021-05-26 11:48:07 +0530

    updated code

--- ExceptionTest.java
+++ ExceptionTest.java
@@ -1,28 +1,56 @@
 import java.util.Scanner;
 
 public class ExceptionTest {
-    public static void validateAge(int age) throws IllegalAgeException {
+
+    public static void validateAge(int age) {
         if(age < 18) {
             throw new IllegalAgeException("Age is too small");
-        } else {
-            System.out.println("Age is valid");   
         }
+        System.out.println("Age is valid");   
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter age:");   
-        int age = sc.nextInt();
+        System.out.print("Enter age:"); int age = sc.nextInt();
+
         try {
             validateAge(age);
         } catch(IllegalAgeException e) {
             System.out.println(e);   
+        } catch(Exception e) {
+            System.out.println(e);   
         }
     }
+
 }
 
-class IllegalAgeException extends Exception {
-    public IllegalAgeException(String msg) {
-        super(msg);
+class IllegalAgeException extends RuntimeException {
+
+    private final int code;
+
+    public IllegalAgeException(String message) {
+        super(message);
+        this.code = 0;
+    }
+
+    public IllegalAgeException(int code) {
+        super();
+        this.code = code;
+    }
+
+    public IllegalAgeException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    public IllegalAgeException(Throwable cause, int code) {
+        super(cause);
+        this.code = code;
+    }
+
+    public IllegalAgeException(String message, Throwable cause, int code) {
+        super(message, cause);
+        this.code = code;
     }
 }
